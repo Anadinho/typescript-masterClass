f0308d749d23adce6871ec78aad2b377e87f48d6
Gabriel Anadinho <gabrielanadinho@gmail.com>
2021-07-12 19:14:40 -0300

    first commit

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response} from 'express';
+import EmailService from '../services/EmailService.';
+
+const users=[
+    {name:'Gabriel',
+     email:'gabrielanadinho@gmail.com'
+    },
+];
+
+export default {
+
+    async index(req: Request, res: Response){
+        return res.json(users);
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+
+        emailService.sendMail(
+            {
+                name:'Gabriel Anadinho',
+                email:'gabrielandinho@gmail.com'
+            },
+            {
+                subject:'Bem-vindo ao sistema',
+                body:'Seja bem vindo'
+            }
+        );
+        return res.send();
+    }
+};
